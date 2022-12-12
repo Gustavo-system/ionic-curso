92340488a6ec95bacc73654b58942a4accc5a6ec
GUSTAVO SOLAR GAONA <gustavo.solar@elektra.com.mx>
2022-12-12 16:50:21 -0600

    se agregan los botonos flotantes - fab

--- 03-componentes/src/app/app-routing.module.ts
+++ 03-componentes/src/app/app-routing.module.ts
@@ -2,43 +2,45 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'inicio',
-    pathMatch: 'full',
-  },
-  {
-    path: 'inicio',
-    loadChildren: () =>
-      import('./pages/inicio/inicio.module').then((m) => m.InicioPageModule),
-  },
-  {
-    path: 'alertas',
-    loadChildren: () =>
-      import('./pages/alertas/alertas.module').then((m) => m.AlertasPageModule),
-  },
-  {
-    path: 'action-sheet',
-    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
-  },
-  {
-    path: 'avatar',
-    loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)
-  },
-  {
-    path: 'buttons',
-    loadChildren: () => import('./pages/buttons/buttons.module').then( m => m.ButtonsPageModule)
-  },
-  {
-    path: 'cards',
-    loadChildren: () => import('./pages/cards/cards.module').then( m => m.CardsPageModule)
-  },
+	{
+		path: '',
+		redirectTo: 'inicio',
+		pathMatch: 'full',
+	},
+	{
+		path: 'inicio',
+		loadChildren: () => import('./pages/inicio/inicio.module').then((m) => m.InicioPageModule),
+	},
+	{
+		path: 'alertas',
+		loadChildren: () => import('./pages/alertas/alertas.module').then((m) => m.AlertasPageModule),
+	},
+	{
+		path: 'action-sheet',
+		loadChildren: () => import('./pages/action-sheet/action-sheet.module').then(m => m.ActionSheetPageModule)
+	},
+	{
+		path: 'avatar',
+		loadChildren: () => import('./pages/avatar/avatar.module').then(m => m.AvatarPageModule)
+	},
+	{
+		path: 'buttons',
+		loadChildren: () => import('./pages/buttons/buttons.module').then(m => m.ButtonsPageModule)
+	},
+	{
+		path: 'cards',
+		loadChildren: () => import('./pages/cards/cards.module').then(m => m.CardsPageModule)
+	},
+	{
+		path: 'fab',
+		loadChildren: () => import('./pages/fab/fab.module').then(m => m.FabPageModule)
+	},
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-  ],
-  exports: [RouterModule],
+	imports: [
+		RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+	],
+	exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- 03-componentes/src/app/pages/inicio/inicio.page.ts
+++ 03-componentes/src/app/pages/inicio/inicio.page.ts
@@ -1,46 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
 interface Components {
-  icon: string;
-  name: string;
-  url: string;
+	icon: string;
+	name: string;
+	url: string;
 }
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+	selector: 'app-inicio',
+	templateUrl: './inicio.page.html',
+	styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-  componentes: Components[] = [
-    {
-      icon: 'american-football-outline',
-      name: 'Action Sheet',
-      url: '/action-sheet',
-    },
-    {
-      icon: 'alert-circle-outline',
-      name: 'Alertas',
-      url: '/alertas',
-    },
-    {
-      icon: 'person-circle-outline',
-      name: 'Avatar',
-      url: '/avatar',
-    },
-    {
-      icon: 'radio-button-off-outline',
-      name: 'Botones',
-      url: '/buttons',
-    },
-    {
-      icon: 'card-outline',
-      name: 'Cards',
-      url: '/cards',
-    },
-  ];
+	componentes: Components[] = [
+		{
+			icon: 'american-football-outline',
+			name: 'Action Sheet',
+			url: '/action-sheet',
+		},
+		{
+			icon: 'alert-circle-outline',
+			name: 'Alertas',
+			url: '/alertas',
+		},
+		{
+			icon: 'person-circle-outline',
+			name: 'Avatar',
+			url: '/avatar',
+		},
+		{
+			icon: 'radio-button-off-outline',
+			name: 'Botones',
+			url: '/buttons',
+		},
+		{
+			icon: 'card-outline',
+			name: 'Cards',
+			url: '/cards',
+		},
+		{
+			icon: 'car-outline',
+			name: 'Fab',
+			url: '/fab',
+		},
+	];
 
-  constructor() {}
+	constructor() { }
 
-  ngOnInit() {}
+	ngOnInit() { }
 }
